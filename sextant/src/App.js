15236fe1aa52b516f361aba9ea33ec404c9b6911
snehamichelle <sneha@facilio.com>
2023-06-25 16:39:16 +0530

    Added banner and components

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Banner">
+        <h1>Sextant</h1>
+      </div>
+      <div className="Component">
+        <div className="ChildComponent1">
+          <h1>This is a child component</h1>
+        </div>
+        <div className="ChildComponent2">
+          <h1>This is the child component</h1>
+        </div>
+      </div>
     </div>
   );
 }
