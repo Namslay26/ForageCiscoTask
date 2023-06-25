7d1abdbacd5d1f0f5518237bf26d1b8267f19e2f
snehamichelle <sneha@facilio.com>
2023-06-25 16:57:11 +0530

    Added tailwindcss

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -1,19 +1,15 @@
 
 import './App.css';
+import Banner from './Banner';
+import Component from './Component';
 
 function App() {
   return (
     <div className="App">
-      <div className="Banner">
-        <h1>Sextant</h1>
-      </div>
-      <div className="Component">
-        <div className="ChildComponent1">
-          <h1>This is a child component</h1>
-        </div>
-        <div className="ChildComponent2">
-          <h1>This is the child component</h1>
-        </div>
+      <Banner title={"Welcome to the title"}></Banner>
+      <div className=" flex">
+        <Component content={'This is a component!'}></Component>
+        <Component content={'This is a component!'}></Component>
       </div>
     </div>
   );
---
+++ sextant/src/Component.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Component({content}) {
+  return (
+    <div className=' px-4 py-2 shadow-lg shadow-gray-300'><p>{content}</p></div>
+  )
+}
